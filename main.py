6ee413092c46598e2aa542b08dba952e7899759e
mirrma <mathis.m16@gamil.com>
2026-05-29 16:15:59 +0200

    first working version

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello API"
+
+@app.route('/add')
+def add():
+    try:
+        a = float(request.args.get('a', 0))
+        b = float(request.args.get('b', 0))
+        result = a + b
+        return jsonify({"a": a, "b": b, "sum": result})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
